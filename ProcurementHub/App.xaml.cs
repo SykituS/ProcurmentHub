2619b4d8eda3b39e557b657429317a48815fcee6
Anime <Anime@DESKTOP-OBB68UN>
2022-12-13 19:09:12 +0100

    Add project files.

---
+++ ProcurementHub/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcurementHub;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ProcurementHub/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProcurementHub;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
