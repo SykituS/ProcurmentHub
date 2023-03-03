7754e6ce5c2c4a1a6fbbf0317d581d20cff10827
Mateusz Jaruga <UL0253575@edu.uni.lodz.pl>
2023-03-03 10:18:56 +0100

    Modifyed script for database

--- SQL/ProcurementHubDataBase.sql
+++ SQL/ProcurementHubDataBase.sql
@@ -10,60 +10,70 @@ GO
 USE ProcurementHub
 GO
 
-CREATE TABLE Users(
+CREATE TABLE Persons(
 	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
 	FirstName VARCHAR(50) NOT NULL,
 	LastName VARCHAR(50) NOT NULL,
 	Email VARCHAR(50) NOT NULL,
-	Password VARCHAR(150) NOT NULL,
-	Role int NOT NULL,
-	Status int NOT NULL,
-	CreatedOn DATETIME NOT NULL,
-	UpdatedOn DATETIME NOT NULL,
-	VerificationCode VARCHAR(6),
-	VerificationStatus int,
-	VerificationCodeExpiry int,
-	ResetCode VARCHAR(50),
-	ResetCodeExpiry DATETIME,
-	IsDeleted bit NOT NULL DEFAULT 0,
-	DeletedDate DATETIME
+)
+
+CREATE TABLE Users(
+	ID nvarchar(128) NOT NULL Primary key,
+	UserName nvarchar(max),
+	PasswordHash nvarchar(max),
+	SecurityStamp nvarchar(max),
+	PersonID int,
+	PasswordChangeToken nvarchar(128),
+	PasswordChangeTokenValidDateTime datetime,
+	PrivacyAgreed bit,
+	PrivacyAgreedOn datetime,
+	Disabled bit not null,
+	VerificationCode nvarchar(6),
+	VerificationCodeValidDateTime datetime,
+	CreatedOn datetime not null,
+	UpdatedOn datetime not null,
+
+	CONSTRAINT FK_User_Person FOREIGN KEY (PersonID) REFERENCES Persons(ID) 
 )
 
 CREATE TABLE Teams (
 	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
 	TeamName VARCHAR(50) NOT NULL,
 	Description VARCHAR(250) NOT NULL,
+	Status int NOT NULL,
 	CreatedByID int NOT NULL,
-	Status int NOT NULL
+	CreatedOn datetime not null,
+	UpdatedByID int not null,
+	UpdatedOn datetime not null,
 
-	CONSTRAINT FK_UserTeam FOREIGN KEY (CreatedByID) REFERENCES Users(ID) 
+	CONSTRAINT FK_Team_PersonCreated FOREIGN KEY (CreatedByID) REFERENCES Persons(ID),
+	CONSTRAINT FK_Team_PersonUpdated FOREIGN KEY (UpdatedByID) REFERENCES Persons(ID) 
 )
 
-CREATE TABLE TeamsMember (
-	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+CREATE TABLE TeamMembers (
 	TeamID int NOT NULL,
-	UserID int NOT NULL,
+	PersonID int NOT NULL,
 	Role int NOT NULL,
 
-	CONSTRAINT FK_TeamTeamMemeber FOREIGN KEY (TeamID) REFERENCES Teams(ID),
-	CONSTRAINT FK_UserTeamMember FOREIGN KEY (UserID) REFERENCES Users(ID)
+	CONSTRAINT FK_TeamMember_Team FOREIGN KEY (TeamID) REFERENCES Teams(ID),
+	CONSTRAINT FK_TeamMember_Person FOREIGN KEY (PersonID) REFERENCES Persons(ID)
 )
 
-CREATE TABLE Restaurants(
-	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
-	Name VARCHAR(100) NOT NULL,
-	Location VARCHAR(200),
-	PhoneNumber int,
-	Website VARCHAR(250),
-)
+--CREATE TABLE Restaurants(
+--	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+--	Name VARCHAR(100) NOT NULL,
+--	Location VARCHAR(200),
+--	PhoneNumber int,
+--	Website VARCHAR(250),
+--)
 
-CREATE TABLE RestaurantItemMenu(
-	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
-	RestaurantID int NOT NULL,
-	ItemName int NOT NULL,
-	ItemPrice MONEY NOT NULL,
-	ItemDescription VARCHAR(250),
-	ItemCategory VARCHAR(150),
+--CREATE TABLE RestaurantItemMenu(
+--	ID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+--	RestaurantID int NOT NULL,
+--	ItemName int NOT NULL,
+--	ItemPrice MONEY NOT NULL,
+--	ItemDescription VARCHAR(250),
+--	ItemCategory VARCHAR(150),
 
-	CONSTRAINT FK_RestaurantItemMenuRestaurant FOREIGN KEY (RestaurantID) REFERENCES Restaurants(ID)
-)
+--	CONSTRAINT FK_RestaurantItemMenuRestaurant FOREIGN KEY (RestaurantID) REFERENCES Restaurants(ID)
+--)
